2965b753a8d8e64e844a389f3d6bd8e53d72a5ff
Vineeth Kumar Boddu <77826030+vineethkumar3@users.noreply.github.com>
2025-05-12 01:07:46 -0400

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
